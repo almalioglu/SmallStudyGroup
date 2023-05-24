8159101ffe5f093468af8a5047fa14a66cf74114
almaliogluis <almalioglu@gmail.com>
2023-05-24 14:22:13 +0300

    SSG1

---
+++ src/main/java/autoBoxingVeUnboxing/C05_autoBoxing.java
@@ -0,0 +1,17 @@
+package autoBoxingVeUnboxing;
+
+public class C05_autoBoxing {
+    //tam sayi olan 2 degeri toplayip double deger olarak dondurun
+
+    public static void main(String[] args) {
+        int sayi=3;
+        int sayi2=7;
+        Integer sayi3=sayi+sayi2;//wrapper classinda doubleValue methodu sayesinde double gibi yazdirdik
+
+        System.out.println(sayi3.doubleValue());
+
+        int a=-65537;
+       short b=(short) a;
+        System.out.println(b);
+    }
+}
